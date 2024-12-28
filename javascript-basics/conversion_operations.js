9cf5ac304e7ca4591266a85a59453b785cb22c0f
abhigyaan17 <abhigyaansharma17@gmail.com>
2024-12-28 18:05:27 +0000

    conversion-datatypes

---
+++ javascript-basics/conversion_operations.js
@@ -0,0 +1,15 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let converted = Number(score)
+// console.log(typeof converted)
+// console.log(converted)
+
+
+let isLoggedIn = "hinduwhore"
+let convertedBoolean = Boolean(isLoggedIn)
+console.log(convertedBoolean)
+// when empty "" boolean is false
+// when not empty "hinduwhore" boolean is true
