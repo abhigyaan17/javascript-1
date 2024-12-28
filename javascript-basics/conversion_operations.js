8fedc5f7c3a9a0e6bd89d39c08376fb7f63ab965
abhigyaan17 <abhigyaansharma17@gmail.com>
2024-12-28 18:27:34 +0000

    conversion-more

--- javascript-basics/conversion_operations.js
+++ javascript-basics/conversion_operations.js
@@ -10,6 +10,36 @@ let converted = Number(score)
 
 let isLoggedIn = "hinduwhore"
 let convertedBoolean = Boolean(isLoggedIn)
-console.log(convertedBoolean)
+// console.log(convertedBoolean)
 // when empty "" boolean is false
 // when not empty "hinduwhore" boolean is true
+
+
+// ----------- OPERATIONS -------------
+
+
+let num = 3
+let negNum = -num
+// console.log(negNum)
+// console.log(2 + 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+
+let str1 = "Hindu"
+let str2 = "Whore"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log(str1 + str2)
+
+
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log(2 + 2 + "1")
+
+let gameCount = 100
+gameCount++
+
+console.log(gameCount)
