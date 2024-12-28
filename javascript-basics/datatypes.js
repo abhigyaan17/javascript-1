dd1e06ead54d16d9ff1d5c142257060e92bf7d4d
abhigyaan17 <abhigyaansharma17@gmail.com>
2024-12-28 17:50:23 +0000

    datatypes-learn

---
+++ javascript-basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat js code in new ver.
+
+// alert( 3 + 3 ) cannot use in node only browser console
+
+console.log(3+3)
+console.log("HinduRandi")
+
+let name = "whore"
+let num = 786
+let isLoggedIn = false
+let state = null
+
+//types of data types://
+
+// number range is from 2 to 2^53
+// bigint
+// string = ""
+// boolean = false or true
+// null is standalone value
+// undefined when value is not defined
+// symbol is used for unique elements
+
+// object
+
+ console.log(typeof undefined)
