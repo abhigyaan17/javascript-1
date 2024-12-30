87d29fbeddf2b3e6410224bcc21ada2b0c73384a
abhigyaan17 <abhigyaansharma17@gmail.com>
2024-12-30 06:02:08 +0000

    strings-upd

---
+++ javascript-basics/strings.js
@@ -0,0 +1,17 @@
+const name = "abhi"
+const age = 19
+
+//console.log(name + age + " male")
+
+// console.log(`My name is ${name} and age is ${age} `);
+
+const ServerName = new String('Acqulus')
+
+console.log(ServerName[2])
+console.log(ServerName.length)
+console.log(ServerName.toUpperCase())
+console.log(ServerName.charAt(2))
+console.log(ServerName.indexOf('u'))
+
+const newString = ServerName.substring(0,4)
+console.log(newString)
