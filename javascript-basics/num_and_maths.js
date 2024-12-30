973b80f5cc76fb4f48f21e85a5b2b520fd11a86d
abhigyaan17 <abhigyaansharma17@gmail.com>
2024-12-30 06:54:05 +0000

    num_maths-upd

---
+++ javascript-basics/num_and_maths.js
@@ -0,0 +1,38 @@
+const balance = 1000
+// console.log(balance)
+
+const balanceNew = new Number(1000)
+
+// console.log(balanceNew.toString().length)
+// console.log(balanceNew.toFixed(2));
+
+const num = 1220.4591
+
+// console.log(num.toPrecision(5));
+
+const amount = 100000
+// console.log(amount.toLocaleString('en-IN'));
+
+// _+_+_+_+_+_+_+_+_+ MATHS _+_+_+_+_+_+_+_+_+_+
+
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.floor(6.9))
+// console.log(Math.min(6,9,12,81))
+// console.log(Math.max(6,9,12,81))
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)+1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
