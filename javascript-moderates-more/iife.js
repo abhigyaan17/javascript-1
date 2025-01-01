28f3a1d0f65e1c3514e9877ca2762ead05c3c6cc
abhigyaan17 <abhigyaansharma17@gmail.com>
2025-01-01 11:43:09 +0000

    iife-info

---
+++ javascript-moderates-more/iife.js
@@ -0,0 +1,9 @@
+//(function-defination)(calling the fucntion)
+(function chai() {
+    console.log("DB Connected");
+    
+})();
+( (name) => {
+    console.log(`DB Connected to ${name}`);
+    }
+)("Abhi");
